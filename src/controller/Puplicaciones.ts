a0df5f89026017a4bfe4e2d76b85212595181559
NAUJVZLA <ju4nc4r1d4d@gmail.com>
2024-08-07 20:14:49 -0500

    ultimos comentarios al codigo ts

--- src/controller/Puplicaciones.ts
+++ src/controller/Puplicaciones.ts
@@ -1,26 +1,40 @@
 import { RespuestaPost, PeticionPost, RespuestaCrearPost, CrearCiudad, RespuestaW } from '../model/func/publicaciones';
 
+// Define la clase PostController que maneja operaciones relacionadas con posts y ciudades
 export class PostController {
 
-    //Create Post
+    // Método para crear un nuevo post
     async createPost(data: PeticionPost): Promise<RespuestaCrearPost> {
+
+        // Obtiene el correo electrónico del usuario logueado desde sessionStorage
         const userLogedEmail = sessionStorage.getItem('x-user-email');
+
+        // Define la URL de la API para crear un post
         const domain: string = 'https://api-posts.codificando.xyz/posts/';
+
+        // Define los encabezados para la solicitud HTTP
         const headers: Record<string, string> = {
             'Content-Type': 'application/json',
             "x-user-email": `${userLogedEmail}`,
         }
+        // Define las opciones para la solicitud HTTP
         const reqOptions: RequestInit = {
             method: 'POST',
             headers: headers,
             body: JSON.stringify(data)
         }
+        // URL completa para la solicitud HTTP
         const url = domain;
+
+        // Realiza la solicitud HTTP a la API
         const result: Response = await fetch(url, reqOptions);
+
+        // Convierte la respuesta en JSON y la asigna a responseBody
         const responseBody: RespuestaCrearPost = await result.json();
 
+        // Verifica si la solicitud fue exitosa (código de estado 201)
         if (result.status === 201) {
-            return responseBody as RespuestaCrearPost
+            return responseBody as RespuestaCrearPost; // Devuelve la respuesta del servidor como RespuestaCrearPost
         } else {
             console.error(`Response body: ${result.status}`);
             throw new Error(`Response body: ${result.status}`);
@@ -28,15 +42,21 @@ export class PostController {
     }
 
 
-    //Get Posts
+    // Método para obtener todos los posts del usuario logueado
     async getPosts(): Promise<Array<RespuestaPost>> {
-        
-        //dato para mostrar de id 
+
+        // Obtiene el ID del usuario desde localStorage
         const idUser = JSON.parse(localStorage.getItem('userIds') || '[]');
         const usuarioLMail = sessionStorage.getItem('x-user-email');
+
+        // Encuentra el ID del usuario logueado en la lista de IDs de usuario
         const userLogedId = idUser.find((user: { email: string }) => user.email === usuarioLMail);
+
+        // Define la URL de la API para obtener los posts del creador
         const urlApi: string = "https://api-posts.codificando.xyz/posts/by-creator/";
         const params: string = `${userLogedId.id}`
+
+        // Define los encabezados para la solicitud HTTP
         const headers: Record<string, string> = {
             "Content-Type": "application/json",
 
@@ -46,11 +66,17 @@ export class PostController {
             method: "GET",
             headers: headers,
         };
+
+        // URL completa para la solicitud HTTP
         const url = urlApi + params;
         const result: Response = await fetch(url, reqOptions);
+
+        // Verifica si la solicitud fue exitosa (código de estado 200)
         if (result.status === 200) {
+
+            // Convierte la respuesta en JSON y la asigna a responseBodyPosts
             const responseBodyPosts: Array<RespuestaPost> = await result.json();
-            return responseBodyPosts;
+            return responseBodyPosts; // Devuelve la lista de posts
         } else if (result.status === 500) {
             throw new Error(`Get Post Error ${result.status}`);
         } else {
@@ -60,38 +86,49 @@ export class PostController {
 
 
 
-    //borrar lugar
+    // Método para eliminar una ciudad de localStorage
     borrarCiudad(name: string): boolean {
         try {
+            // Obtiene la lista de ciudades de localStorage
             const cities = JSON.parse(localStorage.getItem('cities') || '[]');
 
+            // Filtra la lista de ciudades para eliminar la ciudad con el nombre proporcionado
             const updatedCities = cities.filter((city: { info: { name: string } }) => city.info.name.toLowerCase() !== name.toLowerCase());
 
+            // Actualiza localStorage con la lista de ciudades filtrada
             localStorage.setItem('cities', JSON.stringify(updatedCities));
 
+            // Devuelve true si la lista de ciudades ha cambiado (una ciudad fue eliminada), de lo contrario false
             return cities.length !== updatedCities.length;
         } catch (error) {
             throw new Error(`Error DC ${error}`);
         }
     }
 
-    // actualizar
+    // Método para actualizar una ciudad en localStorage
     async actualizaCity(name: string, updatedData: CrearCiudad): Promise<boolean> {
         try {
+            // Obtiene la lista de ciudades de localStorage
             const cities = JSON.parse(localStorage.getItem('cities') || '[]');
 
+            // Encuentra el índice de la ciudad que coincide con el nombre proporcionado
             const index = cities.findIndex((city: { info: { name: string } }) => city.info.name.toLowerCase() === name.toLowerCase());
 
+            // Si se encuentra la ciudad, actualiza su información
             if (index !== -1) {
+                // Obtiene el clima actual de la ciudad
                 const temperature = (await this.getWeather(updatedData.name)).main.temp;
 
+                // Crea un objeto actualizado para la ciudad
                 const updatedCity = {
                     info: updatedData,
                     temperature,
                 };
 
+                // Reemplaza la ciudad antigua con la ciudad actualizada en la lista
                 cities[index] = updatedCity;
 
+                // Actualiza localStorage con la lista de ciudades actualizada
                 localStorage.setItem('cities', JSON.stringify(cities));
 
                 return true;
@@ -104,22 +141,34 @@ export class PostController {
     }
 
 
-    //LUGAR CONTROLS
+    // Método privado para obtener el clima de una ciudad usando una API externa
     private async getWeather(name: string): Promise<RespuestaW> {
+
+        // Define la URL de la API para obtener datos meteorológicos
         const domain: string = '/api/data/2.5/weather';
         const queryParams: string = `?q=${name.toLowerCase()}&appid=4934e44ff1d73160e5749efcbabad009`;
         const headers: Record<string, string> = {
             "Content-Type": "application/json",
         };
+
+        // Define las opciones para la solicitud HTTP
         const reqOptions: RequestInit = {
             method: "GET",
             headers: headers,
         };
+
+        // URL completa para la solicitud HTTP
         const url: string = domain + queryParams;
+
+        // Realiza la solicitud HTTP a la API del clima
         const weatherResponse: Response = await fetch(url, reqOptions);
+
+        // Verifica si la solicitud fue exitosa (código de estado 200)
         if (weatherResponse.ok) {
+
+            // Convierte la respuesta en JSON y la asigna a responseBodyWeather
             const responseBodyWeather: RespuestaW = await weatherResponse.json();
-            return responseBodyWeather;
+            return responseBodyWeather;// Devuelve la información del clima
         } else if (weatherResponse.status == 404) {
             throw new Error(`City not found, status ${weatherResponse.status}`);
         } else {
@@ -127,8 +176,9 @@ export class PostController {
         }
     }
 
-    //Color estado
+    // Método para determinar el color basado en el progreso
     colorEstado(progress: number): string {
+        // Devuelve un color basado en el valor del progreso
         if (progress <= 95) {
             return 'rgb(150, 10, 10)';
         } else {
--- src/controller/usuarioControl.ts
+++ src/controller/usuarioControl.ts
@@ -1,63 +1,96 @@
 import { SolicitarValidacion, RespuestaDeV } from "../model/func/usuarioModelo";
 
+// Define la clase UserController que maneja operaciones de autenticación de usuario
 export class UserController {
+    // Constructor de la clase UserController que acepta dos parámetros opcionales
     constructor(
-        private puntoLOG?: string,
-        private puntoRegistro?: string
+        private puntoLOG?: string, // URL relativa para el endpoint de inicio de sesión (opcional)
+        private puntoRegistro?: string // URL relativa para el endpoint de registro (opcional)
     ) { }
 
-    //Controller Loguin Request Auth
+    // Método para manejar las solicitudes de inicio de sesión (login)
     async postLogin(data: SolicitarValidacion): Promise<RespuestaDeV> {
+        // Define la URL base de la API
         const domain: string = "https://api-posts.codificando.xyz";
+
+        // Define los encabezados para la solicitud HTTP
         const headers: Record<string, string> = {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json",  // Indica que el cuerpo de la solicitud está en formato JSON
         };
+
+        // Define las opciones para la solicitud HTTP
         const reqOptions: RequestInit = {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(data),
+            method: "POST",                     // Método HTTP para enviar datos al servidor
+            headers: headers,                  // Encabezados definidos anteriormente
+            body: JSON.stringify(data),        // Convierte el objeto data a una cadena JSON para el cuerpo de la solicitud
         };
+
+        // Construye la URL completa para la solicitud de inicio de sesión
         const url = domain + this.puntoLOG;
+
+        // Realiza la solicitud HTTP a la API
         const result: Response = await fetch(url, reqOptions);
+
+        // Verifica si la solicitud fue exitosa (código de estado 201)
         if (result.status === 201) {
+
+            // Convierte la respuesta en JSON y la asigna a responseBodyLogin
             const responseBodyLogin: RespuestaDeV = await result.json();
             if (responseBodyLogin.mensaje === 'Login successful') {
-                return responseBodyLogin;
+                return responseBodyLogin; // Devuelve la respuesta del servidor como RespuestaDeV
             } else {
-                throw new Error(responseBodyLogin.mensaje);
+                throw new Error(responseBodyLogin.mensaje); // Lanza una excepción si el mensaje indica un error
             }
         } else {
-            throw new Error(`Login Error ${result.status}`);
+            throw new Error(`Login Error ${result.status}`); // Lanza una excepción si la solicitud no fue exitosa
         }
     }
 
-    //Controler Register Request Auth
+    // Método para manejar las solicitudes de registro (registro de usuario)
     async postRegister(data: SolicitarValidacion): Promise<RespuestaDeV> {
+
+        // Define la URL base de la API
         const domain: string = "https://api-posts.codificando.xyz";
+
+        // Define los encabezados para la solicitud HTTP
         const headers: Record<string, string> = {
             "Content-Type": "application/json",
         };
+
+        // Define las opciones para la solicitud HTTP
         const reqOptions: RequestInit = {
             method: "POST",
             headers: headers,
             body: JSON.stringify(data),
         };
+
+        // Construye la URL completa para la solicitud de registro
         const url = domain + this.puntoRegistro;
+
+        // Realiza la solicitud HTTP a la API
         const result: Response = await fetch(url, reqOptions);
+
+        // Verifica si la solicitud fue exitosa (código de estado 201)
         if (result.status === 201) {
+
+            // Convierte la respuesta en JSON y la asigna a responseBodyRegister
             const responseBodyRegister: RespuestaDeV = await result.json();
-            return responseBodyRegister;
+            return responseBodyRegister; // Devuelve la respuesta del servidor como RespuestaDeV
         } else if (result.status === 500) {
-            throw new Error(`Resgistration Error ${result.status}, probably email was registered before`);
+            throw new Error(`Resgistration Error ${result.status}, probably email was registered before`); // Lanza una excepción si ocurre un error en el servidor (posiblemente por un correo electrónico ya registrado)
         } else {
-            throw new Error(`Registration Error ${result.status}`);
+            throw new Error(`Registration Error ${result.status}`); // Lanza una excepción para otros códigos de error
         }
     }
 
 
-    //control  de la salida 
+    // Método para manejar las solicitudes de cierre de sesión (logout)
     async postLogout(): Promise<RespuestaDeV> {
+
+        // Define la URL base de la API
         const domain: string = "https://api-posts.codificando.xyz";
+
+        // Define los encabezados para la solicitud HTTP
         const headers: Record<string, string> = {
             "Content-Type": "application/json",
         };
@@ -65,14 +98,24 @@ export class UserController {
             method: "POST",
             headers: headers,
         };
+
+        // Construye la URL completa para la solicitud de cierre de sesión
         const url = domain + '/auth/logout';
+
+        // Realiza la solicitud HTTP a la API
         const result: Response = await fetch(url, Ropcion);
+
+        // Verifica si la solicitud fue exitosa (código de estado 201)
         if (result.status === 201) {
+
+            // Convierte la respuesta en JSON y la asigna a RespuestaLogin
             const RespuestaLogin: RespuestaDeV = await result.json();
+
+            // Verifica si el mensaje de respuesta indica éxito
             if (RespuestaLogin.mensaje === 'Logout successful') {
-                sessionStorage.clear();
-                window.location.reload();
-                return RespuestaLogin;
+                sessionStorage.clear();          // Limpia el almacenamiento de sesión
+                window.location.reload();        // Recarga la página para reflejar el estado de cierre de sesión
+                return RespuestaLogin;          // Devuelve la respuesta del servidor como RespuestaDeV
             } else {
                 throw new Error(RespuestaLogin.mensaje);
             }
